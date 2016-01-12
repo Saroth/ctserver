3ecacb7bd94f97b72e02b294b2ff092f0fb0c0fa
huanglf <IronTec@live.cn>
2016-01-12 12:56:04 +0800

    Create project, empty;

---
+++ inc/config.h
@@ -0,0 +1,8 @@
+
+#define DS_DEBUG_MAIN 1                 //!< 调试总开关
+
+#include <libdebug.h>
+
+#define DS_SVR          (DBG_INFO | DBG_LABEL_FUNC | DBG_LABEL_LINE)
+#define DS_SVR_ERR      (DBG_INFO | DBG_LABEL_FUNC | DBG_LABEL_LINE)
+
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <config.h>
+
+int main(int argc, char * argv[])
+{
+    dbg_out_entry(DS_SVR);
+
+    return 0;
+}
+
